e03d1f900151e8c7c7ff969cf6cf0d3a50c3a5b9
yann96E <yannsimonyann96@hotmail.fr>
2022-10-09 20:36:00 -0400

    feat: add changerlacasse

--- ChangerLaCasse/Program.cs
+++ ChangerLaCasse/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace ChangerLaCasse
+{
+    public class Program
+    {
+        public static async Task Main(string[] args) {
+            try {
+                string input = Console.ReadLine();
+                char[] res = new char[input.Length]; 
+                for (int i = 0; i < input.Length; ++i) {
+                    if (Char.IsLetter(input[i]))
+                        res[i] = Char.IsLower(input[i]) ? Char.ToUpper(input[i]) : Char.ToLower(input[i]);
+                    else if (input[i] == ' ')
+                        res[i] = '#';
+                }
+                Console.WriteLine(res);
+            } catch (Exception ex) {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
