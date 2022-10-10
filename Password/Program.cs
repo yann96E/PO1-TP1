3c24db92731eeccfc929057661cb1bb482d16274
yann96E <yannsimonyann96@hotmail.fr>
2022-10-09 21:09:30 -0400

    feat: add password

--- Password/Program.cs
+++ Password/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Password
+{
+    public class Program
+    {
+        public static async Task Main(string[] args) {
+            try {
+                Console.WriteLine(Min(1, 3));
+                Console.WriteLine(Max(0, 4));
+                Console.WriteLine(Chiffre(0, 9));
+                Console.WriteLine(GenerateNPassword(50));
+            } catch (Exception ex) {
+                Console.WriteLine("Error");
+            }
+        }
+        
+        static string Min(int début, int fin) {
+            if (début < 0 || début > 26 || fin < 0 || fin > 26 || fin <= début)
+                return "";
+            return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".Substring(début, (fin - début) + 1);
+        }
 
-Console.WriteLine("Hello, World!");
+        static string Max(int début, int fin)
+        {
+            if (début < 0 || début > 26 || fin < 0 || fin > 26 || fin <= début)
+                return "";
+            return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".Substring(début + 26, (fin - début) + 1);
+        }
+        static string Chiffre(int début, int fin)
+        {
+            return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".Substring(début + 52, (fin - début) + 1);
+        }
+
+        static char[] GenerateNPassword(int n)
+        {
+            string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            char[] password = new char[n];
+            Random aleatoire = new Random();
+            for(int i = 0; i < n; i++)            
+                password[i] = characters[aleatoire.Next(characters.Length)];
+            return password;
+        }
+    }
+}
